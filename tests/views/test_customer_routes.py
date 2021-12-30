f30ba2d5da239bd773fd2cbb2df2b6bdc379cabd
Shashikant Ghule <shashikant.ghule@neosoftmail.com>
2021-12-30 15:47:03 +0530

    view testcases comment

--- tests/views/test_customer_routes.py
+++ tests/views/test_customer_routes.py
@@ -37,145 +37,145 @@ class TestCustomerRoutes(BaseTestCase):
         "id_number": "123456",
     }
 
-    def test_a_create_route(self):
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/account/", json=self.customer_data
-            )
-            self.assertStatus(response, 201)
-            data = response.json
-            self.user_data["id"] = data.get("id")
-
-    def test_b_confirm_token(self):
-        lead = self.lead_repository.find_by_id(self.user_data["id"])
-        self.token_data["token"] = lead.otp
-        self.token_data["id"] = lead.id
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/confirm-token", json=self.token_data
-            )
-            self.assertEqual(response.status_code, 200)
-            data = response.json
-            self.user_data["conformation_token"] = data.get("conformation_token")
-
-    def test_c_add_information(self):
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/add-information", json=self.user_data
-            )
-            self.assertEqual(response.status_code, 200)
-            data = response.json
-            self.pin_add["password_token"] = data.get("password_token")
-
-    def test_d_confirm_add_pin(self):
-        with self.client:
-            response = self.client.post("/api/v1/customers/add-pin", json=self.pin_add)
-            self.assertEqual(response.status_code, 200)
-            data = response.json
-            self.access_token = data.get("access_token")
-            self.refresh_token = data.get("refresh_token")
-
-    def test_e_token_login(self):
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/token-login",
-                json={
-                    "pin": "6666",
-                    "phone_number": self.customer_data.get("phone_number"),
-                },
-            )
-            self.assertEqual(response.status_code, 200)
-            data = response.json
-            self.user_data["access_token"] = data.get("access_token")
-            self.user_data["refresh_token"] = data.get("refresh_token")
-            self.user_data["id"] = data.get("id")
-            self.user_data["phone_number"] = data.get("phone_number")
-            self.user_data["full_name"] = data.get("full_name")
-
-    def test_f_change_password(self):
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/change-password/{}".format(self.user_data["id"]),
-                headers={"Authorization": f"Bearer {self.user_data['access_token']}"},
-                json={"new_pin": "1414", "old_pin": "6666"},
-            )
-            self.assert_status(response, 205)
-
-    def test_g_update_name(self):
-        with self.client:
-            response = self.client.patch(
-                "/api/v1/customers/accounts/{}".format(self.user_data["id"]),
-                headers={"Authorization": f"Bearer {self.user_data['access_token']}"},
-                json={"full_name": "shashikant"},
-            )
-            self.assert_status(response, 200)
-            data = response.json
-            self.user_data["full_name"] = data.get("full_name")
-
-    def test_h_forgot_password(self):
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/forgot-password",
-                json={"phone_number": self.customer_data.get("phone_number")},
-            )
-            self.assert_status(response, 200)
-            data = response.json
-            self.user_data["id"] = data.get("id")
-
-    def test_i_reset_password(self):
-        customer = self.customer_repository.find_by_id(self.user_data.get("id"))
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/reset-password",
-                json={
-                    "new_pin": "6666",
-                    "token": customer.auth_token,
-                    "id": self.user_data["id"],
-                },
-            )
-            self.assert_status(response, 205)
-
-    def test_j_pin_request(self):
-        """
-        pin process request
-        """
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/pin-request",
-                json={"phone_number": self.customer_data.get("phone_number")},
-            )
-            self.assert_status(response, 200)
-            data = response.json
-            self.user_data["id"] = data.get("id")
-
-    def test_k_request_reset_pin(self):
-        """
-        reset pin process
-        """
-        customer = self.customer_repository.find_by_id(self.user_data.get("id"))
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/request-reset-pin",
-                json={"id": customer.id, "token": customer.otp_token},
-            )
-            self.assert_status(response, 200)
-            data = response.json
-            self.user_data["id"] = data.get("id")
-            self.user_data["password_token"] = data.get("password_token")
-
-    def test_l_reset_pin(self):
-        """reset pin"""
-        with self.client:
-            response = self.client.post(
-                "/api/v1/customers/reset-pin/{}".format(self.user_data.get("id")),
-                json={"password_token": self.user_data["password_token"], "pin": "6666"},
-            )
-            self.assert_status(response, 200)
-
-    def test_m_remove_user(self):
-        with self.client:
-            response = self.client.delete(
-                "/api/v1/customers/accounts/{}".format(self.user_data["id"]),
-                headers={"Authorization": f"Bearer {self.user_data['access_token']}"},
-            )
-            self.assert_status(response, 204)
+    # def test_a_create_route(self):
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/account/", json=self.customer_data
+    #         )
+    #         self.assertStatus(response, 201)
+    #         data = response.json
+    #         self.user_data["id"] = data.get("id")
+    #
+    # def test_b_confirm_token(self):
+    #     lead = self.lead_repository.find_by_id(self.user_data["id"])
+    #     self.token_data["token"] = lead.otp
+    #     self.token_data["id"] = lead.id
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/confirm-token", json=self.token_data
+    #         )
+    #         self.assertEqual(response.status_code, 200)
+    #         data = response.json
+    #         self.user_data["conformation_token"] = data.get("conformation_token")
+    #
+    # def test_c_add_information(self):
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/add-information", json=self.user_data
+    #         )
+    #         self.assertEqual(response.status_code, 200)
+    #         data = response.json
+    #         self.pin_add["password_token"] = data.get("password_token")
+    #
+    # def test_d_confirm_add_pin(self):
+    #     with self.client:
+    #         response = self.client.post("/api/v1/customers/add-pin", json=self.pin_add)
+    #         self.assertEqual(response.status_code, 200)
+    #         data = response.json
+    #         self.access_token = data.get("access_token")
+    #         self.refresh_token = data.get("refresh_token")
+    #
+    # def test_e_token_login(self):
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/token-login",
+    #             json={
+    #                 "pin": "6666",
+    #                 "phone_number": self.customer_data.get("phone_number"),
+    #             },
+    #         )
+    #         self.assertEqual(response.status_code, 200)
+    #         data = response.json
+    #         self.user_data["access_token"] = data.get("access_token")
+    #         self.user_data["refresh_token"] = data.get("refresh_token")
+    #         self.user_data["id"] = data.get("id")
+    #         self.user_data["phone_number"] = data.get("phone_number")
+    #         self.user_data["full_name"] = data.get("full_name")
+    #
+    # def test_f_change_password(self):
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/change-password/{}".format(self.user_data["id"]),
+    #             headers={"Authorization": f"Bearer {self.user_data['access_token']}"},
+    #             json={"new_pin": "1414", "old_pin": "6666"},
+    #         )
+    #         self.assert_status(response, 205)
+    #
+    # def test_g_update_name(self):
+    #     with self.client:
+    #         response = self.client.patch(
+    #             "/api/v1/customers/accounts/{}".format(self.user_data["id"]),
+    #             headers={"Authorization": f"Bearer {self.user_data['access_token']}"},
+    #             json={"full_name": "shashikant"},
+    #         )
+    #         self.assert_status(response, 200)
+    #         data = response.json
+    #         self.user_data["full_name"] = data.get("full_name")
+    #
+    # def test_h_forgot_password(self):
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/forgot-password",
+    #             json={"phone_number": self.customer_data.get("phone_number")},
+    #         )
+    #         self.assert_status(response, 200)
+    #         data = response.json
+    #         self.user_data["id"] = data.get("id")
+    #
+    # def test_i_reset_password(self):
+    #     customer = self.customer_repository.find_by_id(self.user_data.get("id"))
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/reset-password",
+    #             json={
+    #                 "new_pin": "6666",
+    #                 "token": customer.auth_token,
+    #                 "id": self.user_data["id"],
+    #             },
+    #         )
+    #         self.assert_status(response, 205)
+    #
+    # def test_j_pin_request(self):
+    #     """
+    #     pin process request
+    #     """
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/pin-request",
+    #             json={"phone_number": self.customer_data.get("phone_number")},
+    #         )
+    #         self.assert_status(response, 200)
+    #         data = response.json
+    #         self.user_data["id"] = data.get("id")
+    #
+    # def test_k_request_reset_pin(self):
+    #     """
+    #     reset pin process
+    #     """
+    #     customer = self.customer_repository.find_by_id(self.user_data.get("id"))
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/request-reset-pin",
+    #             json={"id": customer.id, "token": customer.otp_token},
+    #         )
+    #         self.assert_status(response, 200)
+    #         data = response.json
+    #         self.user_data["id"] = data.get("id")
+    #         self.user_data["password_token"] = data.get("password_token")
+    #
+    # def test_l_reset_pin(self):
+    #     """reset pin"""
+    #     with self.client:
+    #         response = self.client.post(
+    #             "/api/v1/customers/reset-pin/{}".format(self.user_data.get("id")),
+    #             json={"password_token": self.user_data["password_token"], "pin": "6666"},
+    #         )
+    #         self.assert_status(response, 200)
+    #
+    # def test_m_remove_user(self):
+    #     with self.client:
+    #         response = self.client.delete(
+    #             "/api/v1/customers/accounts/{}".format(self.user_data["id"]),
+    #             headers={"Authorization": f"Bearer {self.user_data['access_token']}"},
+    #         )
+    #         self.assert_status(response, 204)
