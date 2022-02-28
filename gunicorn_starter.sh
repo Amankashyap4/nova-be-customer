52402c5ffe099d370fe64825e013ae2191c58787
dharmendra kariya <dharamendra.kariya@gmail.com>
2022-02-28 15:56:34 +0530

    tweaked CI and Gunicorn.sh

--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-gunicorn app.wsgi:app -b 0.0.0.0:5000
+# #!/bin/sh
+# gunicorn app.wsgi:app -b 0.0.0.0:5000
+
+flask db upgrade
+gunicorn app.wsgi:app -b 0.0.0.0:5000 --log-level error --error-logfile gunicorn.error.log --access-logfile log.log
