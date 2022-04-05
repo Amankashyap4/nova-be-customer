60ce6ae6e1abe320578c90242f5fcf81f7aaec8b
Michael Asumadu <michaelasumadu1@gmail.com>
2022-04-05 14:44:29 +0000

    refactor: add package to decode token

--- app/utils/keycloak_fields.py
+++ app/utils/keycloak_fields.py
@@ -1,11 +1,6 @@
 def keycloak_fields(username, obj_data):
     obj_fields = {"username": username}
 
-    if obj_data.get("full_name"):
-        fullname = obj_data.pop("full_name").split(" ")
-        obj_data["first_name"] = fullname[0]
-        obj_data["last_name"] = " ".join(fullname[1:])
-
     for field in obj_data:
         auth_service_field = field.split("_")
         for index in range(len(auth_service_field)):
--- app/utils/validator.py
+++ app/utils/validator.py
@@ -36,3 +36,24 @@ def validate_uuid(obj_id):
             context={"valueError": f"the id {obj_id} is inavlid"}
         )
     return True
+
+
+def split_name(obj_data):
+
+    if obj_data.get("full_name"):
+        fullname = obj_data.pop("full_name").split(" ")
+        obj_data["first_name"] = fullname[0]
+        obj_data["last_name"] = " ".join(fullname[1:])
+    return obj_data
+
+
+def keycloak_fields(username, obj_data):
+    obj_fields = {"username": username}
+
+    for field in obj_data:
+        auth_service_field = field.split("_")
+        for index in range(len(auth_service_field)):
+            if index > 0:
+                auth_service_field[index] = auth_service_field[index].capitalize()
+        obj_fields["".join(auth_service_field)] = obj_data.get(field)
+    return obj_fields
