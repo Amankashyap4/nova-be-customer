099186970d5ff430b241e4cbda74fadc379f0873
Michael Asumadu <michaelasumadu1@gmail.com>
2022-04-08 15:27:11 +0000

    feature: setup profile image update

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,6 @@
 from .auth import auth_required
 from .encoders import JSONEncoder
 from .guid import GUID
+from .image_encoder import save_profile_image, send_profile_image
 from .keycloak_fields import keycloak_fields
 from .validator import validate_uuid, validator
