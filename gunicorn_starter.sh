bb8d8093283a5cfb9cfd54137f88a2ab68343c10
Michael Asumadu <masumadu@archsolutions.online>
2022-05-05 16:17:28 +0000

    feature: add gevent package 	changes: 	- gunicorn is started with gevent 	- gevent spawns new thread for new requests

--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -2,4 +2,4 @@
 
 flask db upgrade
 
-gunicorn app.wsgi:app -b 0.0.0.0:5000 --log-level error --error-logfile - --access-logfile -
+gunicorn app.wsgi:app -b 0.0.0.0:5000 --worker-class gevent --log-level error --error-logfile - --access-logfile -
