ca12575e355e0bdb9e7dd4e9159e256e6bbc753a
Michael Asumadu <masumadu@archsolutions.online>
2022-06-24 13:59:24 +0000

    refactor_kafka: add auth mechanism to consumer

--- app/events/event_data_structure.py
+++ app/events/event_data_structure.py
@@ -3,21 +3,28 @@ import enum
 
 class ServiceEventSubscription(enum.Enum):
     """
+
     This class defines all events subscribed by this service. Events are created in the
-    form: event_to_subscribe = {
-               event_field: [field_trigger(s)]
-          }
+    form: event_name = {data_field: value_of_interest}.Above each event, list
+    various services publishing to such event. Check out the respective service
+    for more info concerning each event.
+
     """
 
+    # nova-be-inventory
     first_time_deposit = {"customer_id": "", "product_name": ""}
+    # nova-be-order
     new_customer_order = {"id": "", "order_by_id": "", "order_status": ""}
 
 
 class ServiceEventPublishing(enum.Enum):
     """
+
     This class defines all events to be published by this service. Events are created in
-    the form: event_name = {data_field: value_of_interest}. Above each event list various
-    service subscribing to such event
+    the form: event_name = {data_field: value_of_interest}. Above each event, list
+    various services subscribing to such event. Check out the respective service
+     for more info concerning each event
+
     """
 
     # nova-be-order, nova-be-inventory
