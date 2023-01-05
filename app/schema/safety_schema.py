8dad06152c2e7bdb2c66af1c82fbf23b04d04e58
Disha1612 <119657094+Disha1612@users.noreply.github.com>
2023-01-05 12:49:00 +0530

    Revert "Feature/nova 53"

--- app/schema/safety_schema.py
+++
@@ -1,6 +0,0 @@
-from marshmallow import Schema, fields, pre_load, validate
-
-class SafetySchema(Schema):
-    title = fields.String(allow_none=True)
-    description = fields.String(allow_none=True)
-    image = fields.String(allow_none=True)
