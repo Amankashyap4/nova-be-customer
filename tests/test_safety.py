5a72d7e5fa6699b6dc29ed62f2d6054af9398b54
admin <sanjay.mehar@hexafoldtech.com>
2023-01-06 13:22:19 +0530

    test-cases

--- tests/test_safety.py
+++ tests/test_safety.py
@@ -1,44 +1,26 @@
-import unittest
-import requests
+from urllib import response
 
-SAFETY_OBJECT={
-    "tittle": "tittle testing",
-    "description" :"description TESTING",
-    "image": "image testing"
-}
+import pytest
 
-class TestSafety(unittest.TestCase):
-    API_URL = "https://customer.newgas.online/api/v1/customers"
-    PROMOTION_URL = "{}/promotion".format(API_URL)
+from app.core import Result
+from app.models.promotion_model import PromotionModel
+from app.repositories.promotion_repository import PromotionRepository
+from tests.base_test_case import BaseTestCase
 
-    SAFETY_OBJECT = {
-        "tittle": "tittle testing",
-        "description": "description TESTING",
-        "image": "image testing"
-    }
 
-    def test_1_get_all_promotion(self):
-        r = requests.get(TestSafety.PROMOTION_URL)
-        self.assertEqual(r.status_code, 200)
+class TestPromotionModel(BaseTestCase):
+    @pytest.mark.model
+    def test_promotion_model(self):
+        self.assertEqual(PromotionModel.query.count(), 0)
 
-    def test_add_new_promotion(self):
-        r = requests.post(TestSafety.PROMOTION_URL,json=TestSafety.SAFETY_OBJECT)
-        self.assertEqual(r.status_code, 200)
 
-    def test_update_promotion(self):
-        NEW_SAFETY_OBJECT = {
-            "tittle": "updated tittle testing",
-            "description": "updated description TESTING",
-            "image": "updated image testing"
-        }
-        id = "171ba65b-2f1d-44d4-9f3d-627783f4495b "
-        r = requests.put("{}/{}".format(TestSafety.PROMOTION_URL,id), json=NEW_SAFETY_OBJECT)
-        self.assertEqual(r.status_code, 405)
 
-    def test_delete_promotion(self):
-        id = "171ba65b-2f1d-44d4-9f3d-627783f4495b "
-        r = requests.delete("{}/{}".format(TestSafety.PROMOTION_URL, id))
-        self.assertEqual(r.status_code, 400)
+class TestSafetyRepository(BaseTestCase):
+    @pytest.mark.repository
+    def test_index(self):
+        result = PromotionRepository.index()
+        self.assertIsInstance(result,Result)
+        self.assertStatus(result, 200)
 
 
 
